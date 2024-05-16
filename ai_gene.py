f0c7462384e15896164792642b0b41aa6c712e3e
TK <tktomo7260t@gmil.com>
2024-05-16 09:47:35 +0900

    chore: Update .gitignore and main.py, and add AI gene code

--- ai_gene.py
+++ ai_gene.py
@@ -1,24 +1,14 @@
-import diffusers, torch
-from diffusers import AutoencoderKL
+from diffusers import StableDiffusionPipeline
+import torch
 
+# パイプラインの準備
+pipe = StableDiffusionPipeline.from_single_file(
+    "ProjectTurn8-Stella-pruned.safetensors",
+    torch_dtype=torch.float16,
+)
 
-#vae = AutoencoderKL.from_single_file("Counterfeit-V2.5.vae.pt")
+# EasyNegativeV2の準備
+pipe.load_textual_inversion("embed/negative",weight_name="EasyNegativeV2.safetensors",token="EasyNegative")
 
-
-pipe = diffusers.StableDiffusionPipeline.from_pretrained("xyn-ai/anything-v4.0",torch_dtype=torch.float16)
-pipe.scheduler = diffusers.DDIMScheduler.from_config(pipe.scheduler.config)
-pipe.load_textual_inversion("EasyNegative-test", weight_name="EasyNegative.safetensors", token="EasyNegative")
-negative_prompt = "EasyNegative"
-
-
-pipe.safety_checker = None
-seeds = [12345]
-steps = 28
-prompt = "girl eating pizza"
-negative_prompt = "EasyNegative, badhandv4"
-for i, seed in enumerate(seeds):
-    print(f"{i}: {seed}")
-    torch.manual_seed(seed)
-    result = pipe(prompt = prompt, negative_prompt = negative_prompt,
-                  width  = 512, height = 512, num_inference_steps = steps)
-    result.images[0].save(f"{seed}-{steps}-ddim-neg.png")
+# NSFWの無効化
+pipe.safety_checker = lambda images, **kwargs: (images, [False] * len(images))
