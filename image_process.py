fe9ac82af02292a2090f434cf20460b476b1065a
TK7 <tktomo7260t@gmail,com>
2024-06-10 03:20:41 +0900

    feat: Improve image processing functionality in image_process.py

--- image_process.py
+++ image_process.py
@@ -1,9 +1,14 @@
 import cv2
 import numpy as np
-def image_process(color,gray,cood,cood_hist):
+
+
+
+def image_process(color,gray,cood,cood_hist,Window):
 
     cood = (int(cood[0]), int(cood[1]))
     
+    print(gray.shape)
+    print(color.shape)
     
 
     height, width = gray.shape[:2]
@@ -12,11 +17,14 @@ def image_process(color,gray,cood,cood_hist):
     radius = 20  # 円の半径
 
     # 円形のマスクを作成
-    mask = np.zeros((height, width), dtype=np.uint8)
+    mask = np.zeros((Window.height, Window.width), dtype=np.uint8)
+
     cv2.line(mask, cood, cood_hist, 255, 20)
 
     # マスクを3チャンネルに拡張（カラー画像用）
     mask_3channel = cv2.merge([mask, mask, mask])
+
+    print(mask_3channel.shape)
 
     img1_bg = cv2.bitwise_and(gray, 255 - mask_3channel)
     img2_fg = cv2.bitwise_and(color, mask_3channel)
--- main.py
+++ main.py
@@ -192,9 +192,6 @@ class PainterScreen(MDScreen):
                 )
             )
         
-        
-
-        
         self.float_layout.add_widget(grid)
         
 
@@ -207,6 +204,8 @@ class PainterScreen(MDScreen):
             if self.collide_point(*touch.pos):
                 self.drawing = True
 
+                 
+
     
     def on_image1_move(self, touch):
         if write_mode == 0:
@@ -214,7 +213,7 @@ class PainterScreen(MDScreen):
         else:
             if self.drawing:
                 try:
-                    process_texture, self.cood_hist = image_process.image_process(self.color_img, self.gray_img, touch.pos, self.cood_hist)
+                    process_texture, self.cood_hist = image_process.image_process(self.color_img, self.gray_img, touch.pos, self.cood_hist, Window)
                     self.gray_img = process_texture
                     texture_canvas = Texture.create(size=(process_texture.shape[1], process_texture.shape[0]), colorfmt='bgr')
                     texture_canvas.blit_buffer(process_texture.tobytes(), colorfmt='bgr')
@@ -223,7 +222,7 @@ class PainterScreen(MDScreen):
                         Rectangle(texture=texture_canvas, pos=(0, 0), size=(process_texture.shape[1], process_texture.shape[0]))
                 except:
                     self.cood_hist = int(touch.pos[0]), int(touch.pos[1])
-                    process_texture , self.cood_hist = image_process.image_process(self.color_img, self.gray_img, touch.pos, self.cood_hist)
+                    process_texture , self.cood_hist = image_process.image_process(self.color_img, self.gray_img, touch.pos, self.cood_hist, Window)
                     self.gray_img = process_texture
                     texture_canvas = Texture.create(size=(process_texture.shape[1], process_texture.shape[0]), colorfmt='bgr')
                     texture_canvas.blit_buffer(process_texture.tobytes(), colorfmt='bgr')
@@ -240,207 +239,26 @@ class PainterScreen(MDScreen):
         else:
             if self.drawing:
                 self.drawing = False
-                del self.cood_hist
-
-    #* 一つ戻す処理
-    def canvas_undo(self):
-        if self.stroke:
-            stroke = self.stroke.pop()
-            self.image_history.pop()
-            self.undo_strokes.append(stroke)
-            self.canvas.remove(stroke)
-
-
-
-    def save_canvas_data(self):
-        if write_mode == 0:
-            canvas_data = []
-            count = 0
-            for i in self.image_history:                
-                canvas_data.append(self.image_history[count])
-                count += 1
-
-            print(canvas_data)
-            return canvas_data
-        else:
-            canvas_data = []
-            count = 0
-            for stroke in self.stroke:
-                color = self.color_history[count]
-                count += 1
-                points = [round(p, 2) for p in stroke.points]
-                width = round(stroke.width, 2)
-                canvas_data.append({'points': points, 'width': width, 'color': color})
-            print(canvas_data)
-            return canvas_data
-
-    def load_canvas_data(self, canvas_data):
-        for stroke in self.stroke:
-            self.canvas.remove(stroke)
-        #キャンバスの色を白にする
-        
-        self.color_history = []
-        self.stroke = []
-        with self.canvas:
-            for stroke_data in canvas_data:
-                Color(rgba=stroke_data['color'])
-                line = Line(points=stroke_data['points'], width=stroke_data['width'])
-                self.stroke.append(line)
-                self.color_history.append(stroke_data['color'])
-    
-
-    
-    def change_color(self):
-        print(self.color_picker)
-    
-
-    def test_code(self):
-        pass
-
-    def load_canvas(self):
-        try:
-            iDir = os.path.abspath(os.path.dirname(__file__))
-            raw_img = tkinter.filedialog.askopenfilename(filetypes=[('Image Files', '*.png')],initialdir=iDir)
-            raw_pickle = raw_img.replace('.png', '.pickle')
-            for stroke in self.stroke:
-                try:
-                    self.canvas.remove(stroke)
-                except:
-                    pass
-                #キャンバスの色を白にする
-                self.color_history = []
-                self.stroke = []
-            save_img = Image.open(raw_img)
-            width_canvas = self.ids.main_canvas.width 
-            hight_root = self.height
-            metadata = save_img.info
-            meta = []
-            height = metadata["height"]
-            width = metadata["width"]
-            canvas_count = int(metadata["count"])
-            
-            #! 画像のサイズが異なる場合は読み込まない  
-            if int(float(hight_root)) != int(float(height)) and int(float(width_canvas)) != int(float(width)):
-                return
-            else:
-                pass
-            
-            f = open(raw_pickle, 'rb')
-            canvas_array = pickle.load(f)
-            f.close()
-            
-            canvas_array = pickle.loads(canvas_array)
-
-            self.stroke = []
-            self.undo_strokes = []
-            self.color_history = []
-            self.save_count = 0
-            self.image_history = [] 
-            self.final_shape = [width , height]
-
-            
-            
-            tmp_count = 0
-            
-            for i in range(canvas_count):
-                texture_canvas = Texture.create(size=(canvas_array[0].shape[1], canvas_array[0].shape[0]), colorfmt='bgr')
-                texture_canvas.blit_buffer(canvas_array[i].tobytes(), colorfmt='bgr')
-
-                
-                with self.canvas:
-                    tmp_rect =  Rectangle(texture=texture_canvas, pos=(0, 0), size=(canvas_array[0].shape[1], canvas_array[0].shape[0])) 
-                    if tmp_count != 0:
-                        self.stroke.append(tmp_rect)
-                    else:
-                        if self.load_state == True:
-                            self.stroke.append(tmp_rect)
-                if tmp_count != 0:
-                    self.image_history.append(canvas_array[i])
-                else:
-                    if self.load_state == True:
-                        self.image_history.append(canvas_array[i])
-                tmp_count += 1
-
-        except FileNotFoundError:
-            print('FileNotFoundError')
-    
-    def save_image_canvas(self):
-        now_time = time.strftime('%Y%m%d%H%M%S', time.localtime())
-        fl_name = './saves/image_' + now_time + '.png'
-        pi_name = './saves/image_' + now_time + '.pickle'
-        
-        width_canvas = self.ids.main_canvas.width 
-        hight_root = self.height
-        self.export_to_png(fl_name)
-        raw_image = Image.open(fl_name)
-        cutted_image = raw_image.crop((0, 0, width_canvas, hight_root))
-        color_rb = cv2.cvtColor(np.array(cutted_image).astype(np.uint8), cv2.COLOR_BGR2RGB)
-        
-        cv2.imwrite(fl_name, np.array(color_rb))
-        
-        tmp_image = Image.open(fl_name)
-
-        metadata = PngInfo()
-        metadata.add_text('data', now_time)
-        canvas_data , canvas_count = self.save_canvas_data()
-        metadata.add_text('height', str(hight_root))
-        metadata.add_text('width', str(width_canvas))
-        metadata.add_text('count', str(canvas_count))
-        
-        f = open(pi_name, 'wb')
-        pickle.dump(canvas_data, f)
-        f.close()
-
-        
-        tmp_image.save(fl_name, pnginfo=metadata)
-
-    def save_canvas_data(self):
-        if write_mode == 0:
-            canvas_data = []
-            count = 0
-            for i in self.image_history:
-                images= self.image_history[count]
-                canvas_data.append(images)
-                count += 1
-            print(count)
-
-            canvas_data = pickle.dumps(canvas_data)
-            
-            return canvas_data , count
-        else:
-            canvas_data = []
-            count = 0
-            for stroke in self.stroke:
-                color = self.color_history[count]
-                count += 1
-                points = [round(p, 2) for p in stroke.points]
-                width = round(stroke.width, 2)
-                canvas_data.append({'points': points, 'width': width, 'color': color})
-            print(canvas_data)
-            return canvas_data
         
     def load_nurie_data(self,id):
         
         
         self.float_layout.clear_widgets()
         
-        self.raw_img = "test6.png"
-        self.bg_img = "sp1.png"
+        
+        self.raw_img = "preview.jpg"
         
         
-        file = cv2.imread(self.raw_img, 0)
-        file2 = cv2.imread(self.bg_img, 1)
-
-        cv2.imwrite('tempp.png', file)
-
-        threshold = 200
+        file = cv2.imread(self.raw_img, 1)
+        
         
         """
         c_width = int(self.ids.main_canvas.width)
         c_height = int(self.height)
         """
-        c_width = int(self.ids.main_canvas.width)
-        c_height = int(self.height)
+        
+        c_width = int(Window.width)
+        c_height = int(Window.height)
         
         print("c_width:" + str(c_width))
         print("c_height:" + str(c_height))
@@ -458,32 +276,54 @@ class PainterScreen(MDScreen):
         else:
             mag = mag_width
         c_width = int(file.shape[1] * mag)
+
+        
         
         
         res_img = cv2.resize(file, dsize = (c_width, c_height))
-        print("res:" + str(c_height) + " " + str(c_width))
 
-        ret, img_thresh = cv2.threshold(res_img, threshold, 255, cv2.THRESH_BINARY)
-        img_thresh = np.where(img_thresh == (0), (40), (255))
+        self.color_img = res_img
+        self.gray_img = cv2.cvtColor(res_img, cv2.COLOR_BGR2GRAY)
+        self.gray_img = np.stack((self.gray_img,)*3, axis=-1)
 
+        width_while = (int(Window.width/2 - self.gray_img.shape[1]/2))
 
-        self.gray_img = cv2.imread(self.raw_img, 1)
-        self.color_img = cv2.imread(self.bg_img, 1)
+        full_img = np.full((Window.height, Window.width),255, dtype=np.uint8)
+        full_img = np.stack((full_img,)*3, axis=-1)
+        
+        dx = width_while   # 横方向の移動距離
+        dy = 0    # 縦方向の移動距離
+        h, w = self.gray_img.shape[:2]
+        full_img[dy:dy+h, dx:dx+w] = self.gray_img
+        
 
-        self.gray_img = cv2.resize(self.gray_img, dsize = (c_width, c_height))
-        self.color_img = cv2.resize(self.color_img, dsize = (c_width, c_height))
+        cv2.imwrite('nurie.png', full_img)
 
         self.gray_img = cv2.flip(self.gray_img, 0)
         self.color_img = cv2.flip(self.color_img, 0)
+
+        full_img = np.full((Window.height, Window.width),255, dtype=np.uint8)
+        full_img = np.stack((full_img,)*3, axis=-1)
+        dx = width_while  # 横方向の移動距離
+        dy = 0    # 縦方向の移動距離
+        h, w = self.gray_img.shape[:2]
+        full_img[dy:dy+h, dx:dx+w] = self.gray_img
+        self.gray_img = full_img
+
         
-        cv2.imwrite('nurie.png', img_thresh)
-        
+        full_img = np.full((Window.height, Window.width),255, dtype=np.uint8)
+        full_img = np.stack((full_img,)*3, axis=-1)
+        dx = width_while   # 横方向の移動距離
+        dy = 0    # 縦方向の移動距離
+        h, w = self.color_img.shape[:2]
+        full_img[dy:dy+h, dx:dx+w] = self.color_img
+        self.color_img = full_img
         
         cv_image = CoreImage.load("./nurie.png")
         cv_image = cv_image.texture
         
         with self.canvas:
-            Rectangle(texture=cv_image, pos=(0, 0), size=(c_width, c_height))
+            Rectangle(texture=cv_image, pos=(0, 0), size=(Window.width, Window.height))
         
         
         self.load_state = True
